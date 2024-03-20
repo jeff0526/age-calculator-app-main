9f19d933285eab7d860ec1748a936f38717765d8
jeff0526 <jepoyaguelo26@gmail.com>
2024-03-19 22:33:31 -0700

    add some animations

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ submit_Btn.addEventListener('click', com_age)
 function com_age(){
     const testme = document.querySelectorAll('.test');
     const d = new Date();
-    
+
     
 
     testme.forEach(
@@ -47,6 +47,7 @@ function com_age(){
         testme[2].nextElementSibling.innerHTML = 'Must be a valid year'
     
     }
+}
      if (!testme[0].value ==''
      &&  !testme[1].value ==''
      && !testme[2].value ==''
@@ -67,37 +68,162 @@ function com_age(){
         yy= Number(yy);
         let newmnth= d.getMonth() ;
         let newdd= d.getDate();
-        newmnth = newmnth +1
-        if (newmnth <= mm && newdd <=dd ){
-            resy.innerHTML = d.getFullYear() - yy - 1;
-            resm.innerHTML =  mm - newmnth;
-            resd.innerHTML = dd - newdd  ;
-            
-        } else if (newmnth <= mm && newdd >dd){
-            resy.innerHTML = d.getFullYear() - yy - 1;
-            resm.innerHTML =  mm - newmnth;
-            resd.innerHTML = newdd - dd  ;
-            
-        } else
-        if(newmnth==mm && newdd >=dd){
-            resy.innerHTML = d.getFullYear() - yy ;
-            resm.innerHTML = 0 ;
-            resd.innerHTML = newdd - dd;
 
-        } else if (newmnth >= mm && newdd <=dd){
-            resy.innerHTML = d.getFullYear() - yy ;
-            resm.innerHTML =  newmnth- mm;
-            resd.innerHTML = dd - newdd ;
+       
+        newmnth = newmnth +1
+        if (newmnth < mm && newdd <=dd ){
+            var numb = 0;
+            var numbm = 0;
+            var numbd = 0;
+            let target = d.getFullYear() - yy - 1;
+            let targetm = 12 - mm + newmnth;
+            let targetd = dd - newdd  ;
+           
+            
+                let myvar= setInterval(myinertval,100)
+                function myinertval(){
+                    if (numb <target){
+                    resy.innerHTML = ++numb;
+                }
+                if (numbm <targetm){
+                    resm.innerHTML = ++numbm;
+                }
+                if (numbd <targetd){
+                    resd.innerHTML = ++numbd;
+                }
+
+            }
+           
+            
+        } else if (newmnth < mm && newdd >dd){
+             var numb = 0;
+            var numbm = 0;
+            var numbd = 0;
+            let target = d.getFullYear() - yy - 1;
+            let targetm = 12 - mm + newmnth;
+            let targetd = newdd - dd  ;
+           
+            
+                let myvar= setInterval(myinertval,100)
+                function myinertval(){
+                    if (numb <target){
+                    resy.innerHTML = ++numb;
+                }
+                if (numbm <targetm){
+                    resm.innerHTML = ++numbm;
+                }
+                if (numbd <targetd){
+                    resd.innerHTML = ++numbd;
+                }
+
+            }
+        } else if(newmnth==mm && newdd >=dd){
+            // resy.innerHTML = d.getFullYear() - yy ;
+            // resm.innerHTML = 0 ;
+            // resd.innerHTML = newdd - dd;
+            var numb = 0;
+            var numbm = 0;
+            var numbd = 0;
+            let target = d.getFullYear() - yy ;
+            let targetm = newmnth- mm;
+            let targetd = newdd - dd  ;
+           
+            
+                let myvar= setInterval(myinertval,100)
+                function myinertval(){
+                    if (numb <target){
+                    resy.innerHTML = ++numb;
+                }
+                if (numbm <targetm){
+                    resm.innerHTML = ++numbm;
+                }
+                if (numbd <targetd){
+                    resd.innerHTML = ++numbd;
+                }
+
+            }
+
+        }  else if(newmnth==mm && newdd < dd){
+            // resy.innerHTML = d.getFullYear() - yy ;
+            // resm.innerHTML = 0 ;
+            // resd.innerHTML = newdd - dd;
+            var numb = 0;
+            var numbm = 0;
+            var numbd = 0;
+            let target = d.getFullYear() - yy - 1;
+            let targetm = newmnth- mm;
+            let targetd = 30 - (dd - newdd)  ;
+           
+            
+                let myvar= setInterval(myinertval,100)
+                function myinertval(){
+                    if (numb <target){
+                    resy.innerHTML = ++numb;
+                }
+                if (numbm <targetm){
+                    resm.innerHTML = ++numbm;
+                }
+                if (numbd <targetd){
+                    resd.innerHTML = ++numbd;
+                }
+
+            }
+
+        }
+        else if (newmnth > mm && newdd <=dd){
+            
+            var numb = 0;
+            var numbm = 0;
+            var numbd = 0;
+            let target = d.getFullYear() - yy ;
+            let targetm = newmnth- mm;
+            let targetd = dd - newdd  ;
+           
+            
+                let myvar= setInterval(myinertval,100)
+                function myinertval(){
+                    if (numb <target){
+                    resy.innerHTML = ++numb;
+                }
+                if (numbm <targetm){
+                    resm.innerHTML = ++numbm;
+                }
+                if (numbd <targetd){
+                    resd.innerHTML = ++numbd;
+                }
+
+            }
         }else
-        if (newmnth >= mm && newdd >dd){
-            resy.innerHTML = d.getFullYear() - yy ;
-            resm.innerHTML =  newmnth- mm;
-            resd.innerHTML = newdd - dd ;
+        if (newmnth > mm && newdd >dd){
+            // resy.innerHTML = d.getFullYear() - yy ;
+            // resm.innerHTML =  newmnth- mm;
+            // resd.innerHTML = newdd - dd ;
+            var numb = 0;
+            var numbm = 0;
+            var numbd = 0;
+            let target = d.getFullYear() - yy ;
+            let targetm = newmnth- mm;
+            let targetd = newdd - dd  ;
+           
+            
+                let myvar= setInterval(myinertval,100)
+                function myinertval(){
+                    if (numb <target){
+                    resy.innerHTML = ++numb;
+                }
+                if (numbm <targetm){
+                    resm.innerHTML = ++numbm;
+                }
+                if (numbd <targetd){
+                    resd.innerHTML = ++numbd;
+                }
+
+            }
         }
        
     }
 }
-}
+
 )
    
 }
